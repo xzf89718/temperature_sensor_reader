f8a5921fd62b253c0eddd3332813d86cda86a242
ZifengXu <zifeng.xu@cern.ch>
2022-11-11 14:09:20 +0800

    Add graphtec_reader.

--- examples/JDRK_sensor.py
+++ examples/JDRK_sensor.py
@@ -1,19 +1,9 @@
 from modbus_configuretools.temperature_sensor import JDRKAddressConfig, RS485_Jiandarenke
 import time
-import logging
-JDRK_sensor_logger = logging.getLogger("JDRK_sensor")
-JDRK_sensor_logger.propagate = False
-JDRK_sensor_logger.setLevel(level=logging.INFO)
-file_handler = logging.FileHandler("JDRK_sensor.log")
-logger_fmt = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-file_handler.setFormatter(logger_fmt)
-JDRK_sensor_logger.addHandler(file_handler)
-JDRK_sensor_logger.info(
-    "Begin read temperature(in degree celsius) dew_point_temperature(in degree celsius) relative_humidity(in %)")
-JDRK_sensor_logger.info(
-    "Recording values is stored in JDRK_sensor.log")
-
-
+from custom_logger import CustomLoggerWrapper
+logger_wrapper = CustomLoggerWrapper("JDRK_sensor", log_filename="MUX64_TC_test.log")
+logger_wrapper.InitLogger()
+JDRK_sensor_logger = logger_wrapper.GetInitedLogger()
 
 ADDRESS_HUMIDITY = 0x0000
 ADDRESS_TEMPERATURE_DEW_POINT = 0x0001
@@ -31,6 +21,10 @@ BAUDRATE = 1
 JDRK_RS485_wangzike_config = JDRKAddressConfig(ADDRESS_HUMIDITY, ADDRESS_TEMPERATURE_DEW_POINT, ADDRESS_TEMPERATURE, ADDRESS_SLAVEID,
                                                ADDRESS_BAUDRATE, ADDRESS_TEMPERATURE_CALI, ADDRESS_HUMUDITY_CALI, TEMPERATURE_CALI, HUMIDITY_CALI, SLAVEID, BAUDRATE)
 
+JDRK_RS485_box_config = JDRKAddressConfig(0x0000, None, 0x0001, ADDRESS_SLAVEID,
+                                               ADDRESS_BAUDRATE, ADDRESS_TEMPERATURE_CALI, ADDRESS_HUMUDITY_CALI, TEMPERATURE_CALI, HUMIDITY_CALI, SLAVEID, BAUDRATE)
+
+
 PORT_NUMBER = "/dev/ttyUSB0"
 PORT_BAUDRATE = 4800
 PORT_TIMEOUT = 1
@@ -40,12 +34,17 @@ mysensor.InitClient()
 
 try:
     while True:
-        temperature, temperature_dewpoint, humidity = mysensor.ReadTemperatureAndHumidity(
-            JDRK_RS485_wangzike_config)
-        _message_to_write = "{0}\t{1}\t{2}".format(
-            temperature, temperature_dewpoint, humidity)
+        # temperature, temperature_dewpoint, humidity = mysensor.ReadTemperatureAndHumidity(
+        #     JDRK_RS485_wangzike_config)
+        # _message_to_write = "{0}\t{1}\t{2}".format(
+        #     temperature, temperature_dewpoint, humidity)
+        temperature = mysensor.ReadTemperature(JDRK_RS485_box_config)[0]
+        humidity = mysensor.ReadHumidity(JDRK_RS485_box_config)[0]
+        _message_to_write = "{0}\t{1}".format(
+            temperature, humidity)
         JDRK_sensor_logger.info(_message_to_write)
-        time.sleep(60)
+        # Get temperature and humidity every 30s
+        time.sleep(30)
 except KeyboardInterrupt as error:
     JDRK_sensor_logger.warning("End write temperature into JDRK_sensor.log")
     mysensor.CloseClient()
